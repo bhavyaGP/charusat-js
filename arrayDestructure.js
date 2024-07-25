6f8d4464984e499393ae0618613843ec689ddbc9
Yagnesh Modh <contact@thespecialcharacter.com>
2024-07-25 11:06:43 +0530

    commit code

---
+++ arrayDestructure.js
@@ -0,0 +1,8 @@
+const data = [1, 2, 3, 4, 5, 9];
+
+const [, , a] = data;
+
+console.log(a);
+
+console.log(data[0]);
+console.log(data[1]);
---
+++ immutableDelete.js
@@ -0,0 +1,5 @@
+const arr = [1, 2, 3, 4, 5];
+
+const [a, b, ...rest] = arr;
+
+console.log(rest);
